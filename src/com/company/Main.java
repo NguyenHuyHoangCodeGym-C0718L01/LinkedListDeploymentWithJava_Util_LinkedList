ba5955ed1b47a0be88be9da3454e8f15cf51a40a
Nguyen Huy Hoang <huyhoang681997@gmail.com>
2018-10-29 21:29:21 +0700

    Hoang finish 'LinkedListDeploymentWithJava_Util_LinkedList' project.

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add(0);
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(1, 4);
+        myLinkedList.addFirst(5);
+        myLinkedList.addLast(6);
+        myLinkedList.remove(1);
+        Integer a = 6;
+        myLinkedList.remove(a);
+        System.out.println(myLinkedList.size());
+        Integer b = 4;
+        System.out.println(myLinkedList.contains(b));
+        Integer c = 3;
+        System.out.println(myLinkedList.indexOf(c));
+        System.out.println(myLinkedList.get(3));
+        System.out.println(myLinkedList.getFirst());
+        System.out.println(myLinkedList.getLast());
+        myLinkedList.clear();
+        System.out.println();
+    }
+}
